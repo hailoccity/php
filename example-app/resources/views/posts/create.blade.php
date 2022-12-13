99af6f80a48f00c2d67f7f3e36adb6670960a57b
HieuLD <ledoanhieu1997@gmail.com>
2022-12-13 22:04:46 +0700

    add index blade

--- example-app/resources/views/posts/create.blade.php
+++ example-app/resources/views/posts/create.blade.php
@@ -1,1 +1,29 @@
-<?php
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between">
+                        <div>Create Post</div>
+                        <div><a href="{{route('posts.index')}}" class="btn btn-success">Back</a></div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="#" method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label for="title">Title :</label>
+                            <input type="text" class="form-control" id="title" placeholder="Enter Title" name="title">
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Title :</label>
+                            <input type="text" class="form-control" id="title" placeholder="Enter Title" name="title">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- example-app/resources/views/posts/index.blade.php
+++ example-app/resources/views/posts/index.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
                         <div class="d-flex justify-content-between">
@@ -10,10 +11,11 @@
                             <div><a href="{{route('posts.create')}}" class="btn btn-success">Create Post</a></div>
                         </div>
                     </div>
+
                     <div class="card-body">
                         <div class="mb-2">
-                            <form class="form-check-inline" action="">
-                                <label for="category_filter">Filter By Category &nbsp;</label>
+                            <form class="d-flex align-items-center d-inline-flex" action="">
+                                <label class="me-3" for="category_filter">Filter By Category</label>
                                 <select class="form-control" id="category_filter" name="category">
                                     <option value="">Select Category</option>
                                 </select>
@@ -23,6 +25,38 @@
                                      <button type="button" class="btn btn-primary">Search</button>
                                       <a class="btn btn-success" href="#">Clear</a>
                             </form>
+                        </div>
+                        <div class="table-responsive">
+                            <table style="width: 100%;" class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Title</th>
+                                    <th>Created By</th>
+                                    <th>Category</th>
+                                    <th>Total Comments
+                                        <a href=""><i class="fas fa-sort-down"></i></a>
+                                        <a href=""><i class="fas fa-sort-up"></i></a>
+                                        <a href=""><i class="fas fa-sort"></i></a>
+                                    </th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td style="width: 35%">Title</td>
+                                    <td>LE HIEU</td>
+                                    <td>Science</td>
+                                    <td class="text-center">2</td>
+                                    <td style="width: 100%;" class="d-flex">
+                                        <a href="#" class="btn btn-primary me-1">View</a>
+                                        <a href="#" class="btn btn-success me-1">Edit</a>
+                                        <a href="#" class="btn btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
