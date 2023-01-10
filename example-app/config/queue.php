e26f2fa8ace77aa2cf4605008222a341c4d8c06e
hyuuneji <hieu11a2>
2023-01-10 17:40:09 +0700

    use queue email

--- example-app/config/queue.php
+++ example-app/config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'sync'),
+    'default' => env('QUEUE_CONNECTION', 'database'),
 
     /*
     |--------------------------------------------------------------------------
@@ -34,8 +34,8 @@ return [
             'driver' => 'sync',
         ],
 
-        'database.sqlite' => [
-            'driver' => 'database.sqlite',
+        'database' => [
+            'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
             'retry_after' => 90,
--- example-app/resources/views/emails/show.blade.php
+++ example-app/resources/views/emails/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-
+<link rel="stylesheet" href="{{asset('css/email.css')}}">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
@@ -13,19 +13,16 @@
                 </div>
                 <div class="card-body">
                     <form action="{{route('emails.demoMail')}}">
-                        <div class="form-control">
-                            <label class="me-2">Người Nhận</label>
-                            <input type="text" name="recipient" class="rounded-2"/>
+                        <div>
+                            <input type="text" name="recipient" placeholder="Người nhận"/>
+                        </div>
+                        <div>
+                            <input type="text" name="subject" placeholder="Chủ đề"/>
                         </div>
                         <div class="form-control">
-                            <label class="me-2">Chủ Đề</label>
-                            <input type="text" name="subject" class="rounded-2"/>
-                        </div>
-                        <div class="form-control">
-                            <label>Chủ Đề</label>
                             <textarea type="" class="form-control" style="width: 100%; height: 300px" id="contents" placeholder="Write Content" name="contents"></textarea>
                         </div>
-                        <button type="submit" class="btn btn-success">Send</button>
+                        <button type="submit" class="btn btn-success mt-3">Send</button>
                     </form>
 
                 </div>
